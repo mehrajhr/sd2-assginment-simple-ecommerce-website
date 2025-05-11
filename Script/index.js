18b2091f2847e04fddad1840e9ee5ef92b975c54
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2025-05-12 01:10:22 +0600

    add addcart btn and handle it

--- Script/index.js
+++ Script/index.js
@@ -15,7 +15,7 @@ const loadProducts = (data) => {
 };
 
 const loadProduct = (element) => {
-    const {id,image,description,title,price} = element;
+  const { id, image, description, title, price } = element;
   console.log(element);
   const cardContainer = document.getElementById("card-container");
   const div = document.createElement("div");
@@ -23,12 +23,13 @@ const loadProduct = (element) => {
     <div class="card bg-base-100  shadow-sm">
   <figure>
     <img
+    id="img_${id}"
       class="h-64"
       src="${image}"
       alt="Shoes" />
   </figure>
   <div class="card-body">
-    <h2 class="card-title">${title}</h2>
+    <h2 id="title_${id}" class="card-title">${title}</h2>
     <p >${description}</p>
     <h4  class="text-2xl font-semibold text-gray-600">Price: <span id="price_${id}">${price}</span></h4>
     <div class="card-actions justify-end">
@@ -36,18 +37,45 @@ const loadProduct = (element) => {
     </div>
   </div>
 </div>
-    `
-    ;
-    cardContainer.appendChild(div);
+    `;
+  cardContainer.appendChild(div);
 };
-const handleAddToCart = (id) =>{
-    totalItem++;
-    document.getElementById('totalItems').innerText = totalItem;
-    document.getElementById("showTotalItem").innerText = totalItem;
-    const price = document.getElementById(`price_${id}`).innerText;
-    console.log(price);
-    const updatePrice = parseFloat(price);
-    console.log(updatePrice);
-    totalPrice += updatePrice;
-    document.getElementById("totalPrice").innerText = totalPrice;
+const handleAddToCart = (id) => {
+  totalItem++;
+  document.getElementById("totalItems").innerText = totalItem;
+  document.getElementById("showTotalItem").innerText = totalItem;
+  const price = document.getElementById(`price_${id}`).innerText;
+  console.log(price);
+  const updatePrice = parseFloat(price);
+  console.log(updatePrice);
+  totalPrice += updatePrice;
+  document.getElementById("totalPrice").innerText = totalPrice;
+  addCardinAddtocardContainer(id,updatePrice);
+};
+const addCardinAddtocardContainer = (id,price) => {
+  const addCardContainer = document.getElementById("addCardContainer");
+  const div = document.createElement("div");
+  const title = document.getElementById(`title_${id}`).innerText;
+  const image = document.getElementById(`img_${id}`).src;
+  div.innerHTML = `
+    <div class="border rounded-2xl p-2 flex gap-4 justify-center items-center w-64">
+        <div>
+                                    <img class="w-16 h-16"
+                                        src="${image}" alt="">
+                                </div>
+                                <div class="flex flex-col gap-2 ">
+                                    <h2>${title}</h2>
+                                    <p>Price : ${price}</p>
+                                </div>
+                            </div>
+    `;
+    addCardContainer.appendChild(div);
+};
+const handleOrderNow = () =>{
+    if(totalItem >= 1){
+        
+        alert("Your order succesfull");
+    }else{
+        alert("no item found,,please add");
+    }
 }
